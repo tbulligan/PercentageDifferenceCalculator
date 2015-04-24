7027b10fd8e7700caa0fe0b0ee8119fa58ae8395
tbulligan <tomaso.bulligan@gmail.com>
2015-04-24 18:56:43 +0200

    Added help link to app

--- ui.R
+++ ui.R
@@ -3,7 +3,10 @@ shinyUI(pageWithSidebar(
     sidebarPanel(
         p("Find out the difference between two numbers"),
         numericInput("num1", "Original number", NULL),
-        numericInput("num2", "New number", NULL)
+        numericInput("num2", "New number", NULL),
+        helpText(a("Click here for help",
+                   href="https://github.com/tbulligan/PercentageDifferenceCalculator/blob/master/README.md",
+                   target="_blank"))
     ),
     mainPanel(
         p("The difference between the numbers is:"),
